4eaced99b71049310a4787c043d1381d3df1fa3a
aditya-mayank <adityamayank11@gmail.com>
2026-05-22 19:41:06 +0545

    chore: remove tests as they are not required for user usage

--- tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import datetime
-from contestpilot.utils import utc_now_iso
-
-def test_utc_now_iso():
-    # Should end with Z or +00:00
-    iso = utc_now_iso()
-    assert isinstance(iso, str)
-    assert 'T' in iso
